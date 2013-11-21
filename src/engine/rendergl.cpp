f0aa2115e8661cc4159b38148e639042e65450cd
Kevin "Hirato Kirata" Meyer <billy65bob@gmail.com>
2013-11-22 01:45:43 +0800

    fix thirdperson camera derp

--- src/engine/rendergl.cpp
+++ src/engine/rendergl.cpp
@@ -1378,9 +1378,9 @@ void recomputecamera()
 
             matrix3 orient;
             orient.identity();
-            orient.rotate_around_z(camera1->yaw*-RAD);
-            orient.rotate_around_x(camera1->pitch*-RAD);
-            orient.rotate_around_y(ovr::modifyroll(camera1->roll)*RAD);
+            orient.rotate_around_z(camera1->yaw*RAD);
+            orient.rotate_around_x(camera1->pitch*RAD);
+            orient.rotate_around_y(ovr::modifyroll(camera1->roll)*-RAD);
             vec dir = vec(orient.b).neg(), side = vec(orient.a).neg(), up = orient.c;
 
             if(game::collidecamera())
