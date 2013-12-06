53e96057c0926c2ff1ef0dd77b947dbb68a3afcd
Kevin "Hirato Kirata" Meyer <billy65bob@gmail.com>
2013-12-07 00:25:56 +0800

    check for win64 bins in launch bat

--- lamiae.bat
+++ lamiae.bat
@@ -1,1 +1,14 @@
-start binw32\lamiae.exe "-q$HOME\My Games\Lamiae" -gstdout.txt %*
+﻿@ECHO OFF
+
+set LAMIAE_BIN=binw32
+
+IF EXIST binw64\lamiae.exe (
+    IF /I "%PROCESSOR_ARCHITECTURE%" == "amd64" (
+        set LAMIAE_BIN=binw64
+    )
+    IF /I "%PROCESSOR_ARCHITEW6432%" == "amd64" (
+        set LAMIAE_BIN=binw64
+    )
+)
+
+start %LAMIAE_BIN%\lamiae.exe "-q$HOME\My Games\Lamiae" -gstdout.txt %*
