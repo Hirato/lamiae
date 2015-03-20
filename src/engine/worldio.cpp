Hirato/lamiae

commit 7e69c7df7d9490c48e788a92036ff34b2980a2ac

Initial import of revision r1952
eihrul (209):
      allow vcolor and texcolor to work up to 2x range
      decalload for loading .dcl configs
      use .cfg instead of .dcl for decal config extension
      fix sound toggling
      workaround for winapifamily.h problems
      deferred light shader cleanups
      cheesy volumetric lighting prototype
      add volumetric light shaders
      clamp volumetric prefilter distance
      dynlightreaching fix
      volshadow doesn't work, just remove for now
      blur volumetric lights
      volumetric light shader fixes
      better volumetric light dithering
      volumetric light gather fixes
      volumetric spotlight optimizations
      more volumetric spotlight optimizations
      rename kill to suicide
      ragdoll initialization fix
      swap volumetric textures each frame
      allow vslots in edit mode
      make assignvslotlayers inline
      simplifying layer/decal packing
      unpacktex cleanup
      expose light flags to dynlights
      vdecal/vlayer packing fixes
      mark vslotrefs as unused
      pasteundoents cleanup
      multiplayer undo
      visual studio fixes
      more visual studio fixes
      higher volumetric blur threshold
      fix admin/auth/master checks
      wtris fix
      server edit filter tweaks
      setupvolumetric fix
      postfx cleanup
      blurshader cleanups
      volumetric blur cleanups
      make volcolour CVAR1R
      better volumetric light bilateral filtering
      volumetric spotlight fixes
      pasteundoblock fix
      replace base/black with base/glow in default map textures
      add missing glow.tex
      smaller avatardepth
      add tex files for pk2 and missing floor13 texture
      add philipk2 textures to default map settings
      decal shader glow fix
      decal glow fixes
      decal glow shader cleanup
      more decal glow shader cleanups
      decal glow shader typo fix
      fixqtangent bias scale fix
      lower default spec scale

src/engine/worldio.cpp
@@ -472,8 +472,8 @@ void saveslotconfig(stream *h, Slot &s, int index)
             h->printf("texscale %g\n", vs.scale);
         if(vs.layer != 0)
             h->printf("texlayer %d\n", vs.layer);
-        if(vs.decal != 0)
-            h->printf("texdecal %d\n", vs.decal);
+        if(vs.detail != 0)
+            h->printf("texdetail %d\n", vs.detail);
         if(vs.alphafront != 0.5f || vs.alphaback != 0)
             h->printf("texalpha %g %g\n", vs.alphafront, vs.alphaback);
         if(vs.colorscale != vec(1, 1, 1))
@@ -584,7 +584,7 @@ void savevslot(stream *f, VSlot &vs, int prev)
         f->putlil<float>(vs.refractscale);
         loopk(3) f->putlil<float>(vs.refractcolor[k]);
     }
-    if(vs.changed & (1<<VSLOT_DECAL)) f->putlil<int>(vs.decal);
+    if(vs.changed & (1<<VSLOT_DETAIL)) f->putlil<int>(vs.detail);
 }
 
 void savevslots(stream *f, int numvslots)
@@ -661,7 +661,7 @@ void loadvslot(stream *f, VSlot &vs, int changed)
         vs.refractscale = f->getlil<float>();
         loopk(3) vs.refractcolor[k] = f->getlil<float>();
     }
-    if(vs.changed & (1<<VSLOT_DECAL)) vs.decal = f->getlil<int>();
+    if(vs.changed & (1<<VSLOT_DETAIL)) vs.detail = f->getlil<int>();
 }
 
 void loadvslots(stream *f, int numvslots)

commit a9cba5acd51a37330d0eda6effad29b256a3833b

fix world saving, which has apparently been broken since 7e69c7df7d9490c48e788a92036ff34b2980a2ac mid last year

src/engine/worldio.cpp
@@ -822,7 +822,7 @@ bool save_world(const char *mname, bool nolms, bool octa)
     savemapprogress = 0;
     renderprogress(0, "saving map...");
 
-    octaheader hdr;
+    mapheader hdr;
     memcpy(hdr.magic, (octa ? "TMAP" : "MLAM"), 4);
     hdr.version = octa ? TESSMAPVERSION : LAMIAEMAPVERSION;
     hdr.headersize = sizeof(hdr);